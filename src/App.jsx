2a20e53dba9a091ca16154811d3b49e175e35dbe
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-11-28 13:14:43 +0530

    :zap: :art: Polling logic implemented with individual vote count

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import "./App.css";
 import PollComponent from "./components/PollComponent";
 
+const QUESTIONS_WITH_OPTIONS = [
+  {
+    id: 1,
+    question: "Sample question 1 ?",
+    options: [
+      { option: "A", count: 0 },
+      { option: "B", count: 0 },
+      { option: "C", count: 0 },
+      { option: "D", count: 0 },
+    ],
+  },
+  {
+    id: 2,
+    question: "Sample question 2 ?",
+    options: [
+      { option: "A", count: 0 },
+      { option: "B", count: 0 },
+      { option: "C", count: 0 },
+      { option: "D", count: 0 },
+    ],
+  },
+];
+
 function App() {
   return (
     <>
-      <PollComponent />
+      <PollComponent questions={QUESTIONS_WITH_OPTIONS} />
     </>
   );
 }
--- src/components/PollComponent.jsx
+++ src/components/PollComponent.jsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
 
-const PollComponent = () => {
+const PollComponent = ({ questions }) => {
+  const [questionair, setQuestionair] = useState(questions);
+
+  const changeCount = (questionIndex, optionIndex) => {
+    const updatedVotes = { ...questionair };
+    updatedVotes[questionIndex].options[optionIndex].count += 1;
+    setQuestionair({ ...updatedVotes });
+  };
+
   return (
-    <div>PollComponent</div>
-  )
-}
+    <div>
+      {questions.map(({ question, options }, questionIndex) => (
+        <div key={`question-${questionIndex}`} className="question-container">
+          <h4 className="question-title" id={`question-${questionIndex}`}>
+            {question}
+          </h4>
+          <div className="options-container">
+            {options.map(({ option, count }, optionIndex) => (
+              <button
+                key={`option-${questionIndex}-${optionIndex}`}
+                className="option-button"
+                onClick={() => changeCount(questionIndex, optionIndex)}
+              >
+                <span className="option-text">{option}</span>
+                <br />
+                <span className="vote-count">Vote {count}</span>
+              </button>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default PollComponent
+export default PollComponent;
